c01397467c5656a06df72236d986153251346c13
renzdimayuga <renz574@gmail.com>
2016-03-28 12:08:06 +0800

    x
    x

---
+++ Assets/TableRoll.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TableRoll : MonoBehaviour {
+
+	private float turnSpeed = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rX = transform.eulerAngles.x;
+		float rY = transform.eulerAngles.y;
+		float rZ = transform.eulerAngles.z;
+		if (rX > 10 && rX < 350) {
+			if (rX < 180) {
+				transform.Rotate (Vector3.right, 10 - rX);
+			} else {
+				transform.Rotate (Vector3.right, 350 - rX);
+			}
+		}
+		if (rZ > 10 && rZ < 350) {
+			if (rZ < 180) {
+				transform.Rotate (Vector3.forward, 10 - rZ);
+			} else {
+				transform.Rotate (Vector3.forward, 350 - rZ);
+			}
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Rotate (Vector3.right, turnSpeed);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Rotate (Vector3.right, -turnSpeed);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (Vector3.forward, -turnSpeed);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (Vector3.forward, turnSpeed);
+		}
+	}
+}
