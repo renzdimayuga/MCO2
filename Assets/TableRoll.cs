92a3e0de9047a3b2aaf8c2cb90a3b2f60a7caa66
markgromantigue <markg.romantigue@gmail.com>
2016-04-01 14:14:38 +0800

    Update table Roll

--- Assets/TableRoll.cs
+++ Assets/TableRoll.cs
@@ -11,35 +11,7 @@ public class TableRoll : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		float rX = transform.eulerAngles.x;
-		float rY = transform.eulerAngles.y;
-		float rZ = transform.eulerAngles.z;
-		if (rX > 10 && rX < 350) {
-			if (rX < 180) {
-				transform.Rotate (Vector3.right, 10 - rX);
-			} else {
-				transform.Rotate (Vector3.right, 350 - rX);
-			}
-		}
-		if (rZ > 10 && rZ < 350) {
-			if (rZ < 180) {
-				transform.Rotate (Vector3.forward, 10 - rZ);
-			} else {
-				transform.Rotate (Vector3.forward, 350 - rZ);
-			}
-		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			transform.Rotate (Vector3.right, turnSpeed);
-		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			transform.Rotate (Vector3.right, -turnSpeed);
-		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.Rotate (Vector3.forward, -turnSpeed);
-		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.Rotate (Vector3.forward, turnSpeed);
-		}
-	}
+	void run(){
+    
+    }
 }
